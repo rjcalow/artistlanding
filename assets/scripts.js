d087baf28fd97c8226b8c07ad802034718a98270
Richard Calow <r.j.calow@gmail.com>
2021-07-05 20:15:47 +0100

    effects, scripts, css

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,7 @@
 function w3_open(y) {
-  document.getElementById("imginfo").classList.add("hidden")
+  closemenu();
+
+  document.getElementById("imginfo").classList.add("hidden");
   var x = document.getElementById(y);
 
   if (x.classList.contains("fadein")) {
@@ -48,27 +50,24 @@ window.addEventListener("wheel", function (scroll) {
 
 // var intervalID = window.setInterval(background, 2000);
 var i = 0;
-var BackgroundImg = ["assets/bk.jpg", "assets/bk2.jpg", "assets/bk3.jpg"];
 
-
-var Key = [1,2,3,4,5,6,7];
-
-var Images = ["assets/constructed/RJCALOW-altcamerainlandscape-2.jpg",
- "assets/darkpeak/RJCALOW_peak_district_holga-8.jpg",
- "assets/pandemicsketchbook/14.jpg",
-"assets/snag/2.jpg",
-"assets/unpitched/0007.jpg",
-"assets/zone.gif"]
+var Images = [
+  "assets/constructed/RJCALOW-altcamerainlandscape-2.jpg",
+  "assets/darkpeak/RJCALOW_peak_district_holga-8.jpg",
+  "assets/pandemicsketchbook/14.jpg",
+  "assets/snag/2.jpg",
+  "assets/unpitched/0007.jpg",
+  "assets/zone.gif",
+];
 
 var Imginfo = [
-'<a href="https://rjcalow.co.uk/constructed">Constructed</a>',
-'<a href="https://rjcalow.co.uk/darkpeak">Dark Peak</a>',
-'<a href="https://rjcalow.co.uk/pandemicsketchbook">Pandemic Sketchbook</a>',
-' <a href="https://rjcalow.co.uk/snag">Snag / Inflorescence</a>',
-'<a href="https://rjcalow.co.uk/unpitched">Unpitched</a>',
-' <a href="https://rjcalow.co.uk/zone">Zone</a>'
-]
-
+  '<a href="https://rjcalow.co.uk/constructed">Constructed</a>',
+  '<a href="https://rjcalow.co.uk/darkpeak">Dark Peak</a>',
+  '<a href="https://rjcalow.co.uk/pandemicsketchbook">Pandemic Sketchbook</a>',
+  ' <a href="https://rjcalow.co.uk/snag">Snag / Inflorescence</a>',
+  '<a href="https://rjcalow.co.uk/unpitched">Unpitched</a>',
+  ' <a href="https://rjcalow.co.uk/zone">Zone</a>',
+];
 
 function background(x) {
   if (document.getElementById("background").classList.contains("fadeinfast")) {
@@ -77,11 +76,11 @@ function background(x) {
   i = i + x;
   if (i >= 6) {
     i = 0;
-  } else if(i< 0){
+  } else if (i < 0) {
     i = 5;
   }
-  console.log(i)
-  document.getElementById("imginfo").classList.remove("hidden")
+
+  document.getElementById("imginfo").classList.remove("hidden");
   document.getElementById("background").classList.add("fadeinfast");
   document.getElementById("background").style.backgroundImage =
     'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)),url("' +
@@ -99,12 +98,59 @@ function removefade() {
 }
 
 function changebk(x) {
-  document.getElementById("imginfo").classList.add("hidden")
+  document.getElementById("imginfo").classList.add("hidden");
   document.getElementById("background").style.backgroundImage =
-    'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.2)),url("' +
-    x +
-    '")';
+    'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.1)),url("' + x + '")';
+}
 
+function menu() {
+  apply_class("imginfo", "hidden");
+  apply_class("icon", "rotate");
+  var x = document.getElementById("menu");
+
+  if (window.screen.width > 801) {
+    x.style.display = "block";
+    if (x.style.top === "10px") {
+      closemenu();
+    } else {
+      x.style.top = "10px";
+    }
+  } else if (window.screen.width < 800) {
+     
+    x.style.top = "0px";
+    if (x.style.display === "block") {
+
+      closemenu();
+    } else {
+      apply_class("menu", "fadeinfast");
+      x.style.display = "block";
+    }
+  }
+
+}
+
+function closemenu() {
+  var x = document.getElementById("menu");
+  remove_class("menu", "fadeinfast");
+  remove_class("links", "swipe_down")
+  remove_class("links", "swipe_up")
+  remove_class("icon", "rotate");
+  if (window.screen.width < 800) {
+    x.style.display = "none";
     
+  } else {
+    x.style.top = "-150px";
+  }
+}
 
+
+
+function apply_class(id, class_) {
+  var x = document.getElementById(id);
+  x.classList.add(class_);
+}
+
+function remove_class(element, class_) {
+  var x = document.getElementById(element);
+  x.classList.remove(class_);
 }
